c17f3e290da5180448f10f2c76997dcf1b224f35
qiujiangkun <qiujiangkun@foxmail.com>
2021-07-28 18:30:13 +0800

    integrate astor basically

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,21 +9,23 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.0"
+    id("org.jetbrains.intellij") version "1.1.2"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.1.2"
 }
 
 group = properties("pluginGroup")
 version = properties("pluginVersion")
-
+//
 // Configure project's dependencies
 repositories {
     mavenCentral()
+    mavenLocal()
 }
 dependencies {
-
+    implementation("org.inria.sacha.automaticRepair:astor:1.1.0")
 }
+
 
 // Configure gradle-intellij-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-intellij-plugin
--- src/main/kotlin/org/bytecamp/program_repair/astor_plugin/actions/AstorHelloAction.kt
+++ src/main/kotlin/org/bytecamp/program_repair/astor_plugin/actions/AstorHelloAction.kt
@@ -2,10 +2,13 @@ package org.bytecamp.program_repair.astor_plugin.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.service
 import com.intellij.openapi.ui.Messages
+import org.bytecamp.program_repair.astor_plugin.services.AstorProjectService
 
 class AstorHelloAction: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        Messages.showMessageDialog(e.project, "Hello", "Astor", null)
+        val service = e.project?.service<AstorProjectService>()
+        Messages.showMessageDialog(e.project, "Hello<br>" + service?.getHelp(), "Astor", null)
     }
 }
