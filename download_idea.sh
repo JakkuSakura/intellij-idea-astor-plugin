85a373d7980c11f571196acf977af96b9bbbb615
qiujiangkun <qiujiangkun@foxmail.com>
2021-07-28 14:52:10 +0800

    add source code

--- download_idea.sh
+++ download_idea.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p downloaded
 wget -q -nc https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/ideaIC/2021.2/ideaIC-2021.2.zip -O downloaded/ideaIC-2021.2.zip
-unzip downloaded/ideaIC-2021.2.zip -d downloaded/ideaIC-2021.2
+wget -q -nc https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/ideaIC/2021.2/ideaIC-2021.2-sources.jar -O downloaded/ideaIC-2021.2-sources.jar
+unzip -n downloaded/ideaIC-2021.2.zip -d downloaded/ideaIC-2021.2
