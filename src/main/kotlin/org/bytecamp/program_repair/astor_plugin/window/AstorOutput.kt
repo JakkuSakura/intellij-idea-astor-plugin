5959751aa363d58257a5f41388ed481bf29e7c71
cjinchi <cjinchi@qq.com>
2021-08-04 22:48:11 +0800

    disable line wrap, color logging level

--- src/main/kotlin/org/bytecamp/program_repair/astor_plugin/window/AstorOutput.kt
+++ src/main/kotlin/org/bytecamp/program_repair/astor_plugin/window/AstorOutput.kt
@@ -6,26 +6,43 @@ import javax.swing.JTextArea
 import javax.swing.JTextField
 import javax.swing.JTextPane
 import javax.swing.text.Document
+import javax.swing.text.AttributeSet
+import javax.swing.text.SimpleAttributeSet
+import javax.swing.text.StyleConstants
+import javax.swing.text.StyleContext
 
+import java.awt.Color
 
-class AstorOutput: JPanel() {
-    var text = JTextArea()
+class AstorOutput : JPanel() {
+    var text = JTextPane()
     var jsp = com.intellij.ui.components.JBScrollPane(text)
+    val sc = StyleContext.getDefaultStyleContext()
+    val style = sc.addStyle("font folor", null)
 
     init {
         layout = BorderLayout()
         text.isEditable = false
         text.autoscrolls = true
-        text.lineWrap = true
+//        text.lineWrap = true
         super.add(jsp)
     }
+
     fun clear() {
         text.text = ""
     }
+
     fun appendText(str: String) {
+        println(str)
         val document = text.document
-        document.insertString(document.length, str, null)
+        var c: Color
+        when (str) {
+            "DEBUG" -> c = Color.BLUE
+            "INFO" -> c = Color.GREEN
+            "ERROR" -> c = Color.RED
+            else -> c = Color.WHITE
+        }
+        StyleConstants.setForeground(style, c)
+        document.insertString(document.length, str, style)
         text.caretPosition = document.length
     }
-
 }
